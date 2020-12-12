531b17294a21b96c8b9eefd2fd1f47c9e9df5811
Rilshrink <75235658+Rilshrink@users.noreply.github.com>
2020-12-12 06:11:59 -0600

    KauriDisabler.java

---
+++ KauriDisabler.java
@@ -0,0 +1,16 @@
+public void KauriDisabler extends Module {
+    public KauriDisabler() {
+        super("Disabler", "Disables Kauri Anticheat", Catagory.MISC);
+    }
+    public void onEnable() {
+        if(!mc.isSinglePlayer()) {
+            Client.addChatMessage("[Disabler] §4Relog for disabler to work.");
+        }
+    }
+    public void onPacket(EventPacket event) {
+        if(!this.getState()) return;
+        
+        if(event.getPacket() instanceof C0FPacketConfirmTransaction) event.setCanceled(true):
+        
+    }
+}
